1dedf7fded4de1ccb025d2d3053bf6f072f46357
Marcelo Bezerra <bezerra.mj@gmail.com>
2020-02-27 14:22:18 -0300

    Services - Usando injeçao de dependencia corretamente, por meio de interfaces (reduzindo acoplamento da implementaçao concreta)
    HttpPulsarPay - classe muito grande, refatorei e quebrei em classes menores e mais especificas (boleto, cliente e produto)

--- src/main/java/br/com/ericsoncbizarro/IntegracaoPulsarPay/services/ClienteService.java
+++ src/main/java/br/com/ericsoncbizarro/IntegracaoPulsarPay/services/ClienteService.java
@@ -1,36 +1,11 @@
 package br.com.ericsoncbizarro.IntegracaoPulsarPay.services;
 
-import br.com.ericsoncbizarro.IntegracaoPulsarPay.services.httpPulsarPay.HttpPulsarPay;
 import br.com.ericsoncbizarro.IntegracaoPulsarPay.model.modelPulsarPay.Cliente;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
-
 import java.util.List;
-import java.util.concurrent.atomic.AtomicLong;
 
-@Service
-public class ClienteService {
+public interface ClienteService {
 
-    @Autowired
-    private HttpPulsarPay httpPulsarPay;
+    List<Cliente> findAll() throws Exception;
+    List<Cliente> create(Cliente clienteRequestBody) throws Exception;
 
-    private final AtomicLong counter = new AtomicLong();
-
-    public List<Cliente> findAll() throws Exception {
-        return httpPulsarPay.getClientes();
-    }
-
-    public List<Cliente> create(Cliente clienteRequestBody) throws Exception { // criar um dto para cliente
-        Cliente clienteMock = mockCliente(clienteRequestBody);
-        return httpPulsarPay.postCliente(clienteMock);
-    }
-
-    private Cliente mockCliente(Cliente clienteRequestBody){
-        Cliente clienteHttp = new Cliente();
-        clienteHttp.setControle_externo(counter.incrementAndGet());
-        clienteHttp.setNome(clienteRequestBody.getNome());
-        clienteHttp.setDocumento(clienteRequestBody.getDocumento());
-        clienteHttp.setData_nascimento(clienteRequestBody.getData_nascimento());
-        return clienteHttp;
-    }
 }
---
+++ src/main/java/br/com/ericsoncbizarro/IntegracaoPulsarPay/services/impl/BoletoServiceImpl.java
@@ -0,0 +1,33 @@
+package br.com.ericsoncbizarro.IntegracaoPulsarPay.services.impl;
+
+import br.com.ericsoncbizarro.IntegracaoPulsarPay.model.modelPulsarPay.Boleto;
+import br.com.ericsoncbizarro.IntegracaoPulsarPay.services.BoletoService;
+import br.com.ericsoncbizarro.IntegracaoPulsarPay.services.HttpPulsarPayBoleto;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import java.util.List;
+
+@Service
+public class BoletoServiceImpl implements BoletoService {
+
+    @Autowired
+    private HttpPulsarPayBoleto httpPulsarPay;
+
+    public List<Boleto> findAll() throws Exception {
+        return httpPulsarPay.getBoletos();
+    }
+
+    public List<Boleto> create(Boleto boletoRequestBody) throws Exception { // criar um dto para cliente
+        Boleto boletoMock = mockBoleto(boletoRequestBody);
+        return httpPulsarPay.postBoleto(boletoMock);
+    }
+
+    private Boleto mockBoleto(Boleto boletoRequestBody){
+        Boleto boletoHttp = new Boleto();
+        boletoHttp.setValor_total(boletoRequestBody.getValor_total());
+        boletoHttp.setDocumento(boletoRequestBody.getDocumento());
+        boletoHttp.setData_nascimento(boletoRequestBody.getData_nascimento());
+        boletoHttp.setVencimento(boletoRequestBody.getVencimento());
+        return boletoHttp;
+    }
+}
